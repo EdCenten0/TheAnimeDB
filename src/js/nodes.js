0eac002aa9ad0d145215d39db1e36afad5de6103
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-08-13 00:05:53 -0600

    Nodes added

---
+++ src/js/nodes.js
@@ -0,0 +1,29 @@
+const body = document.getElementById("body");
+const returnArrow = document.getElementById("returnArrow");
+const headerPrincipal = document.getElementById("headerPrincipal");
+const mainContentDescription = document.getElementById(
+  "mainContentDescription"
+);
+const contentAside = document.getElementById("contentAside");
+const contentTitle = document.getElementById("contentTitle");
+const contentSubTitle = document.getElementById("contentSubTitle");
+const contentInfo = document.getElementById("contentSubTitle");
+const categotyPreviewContainer = document.getElementById(
+  "categotyPreviewContainer"
+);
+const categoriesPreviewTitle = document.getElementById(
+  "categoriesPreviewTitle"
+);
+const categoryPreviewList = document.getElementById("categoryPreviewList");
+const categoryContainer = document.getElementById("categoryContainer");
+const genericList = document.getElementById("genericList");
+const genericListHeader = document.getElementById("genericListHeader");
+const genericListTitle = document.getElementById("genericListTitle");
+const genericListButton = document.getElementById("genericListButton");
+const genericListCarousel = document.getElementById("genericListCarousel");
+const genericListCardContainer = document.getElementById(
+  "genericListCardContainer"
+);
+const cardImage = document.getElementById("cardImage");
+const footer = document.getElementById("footer");
+console.log(body);
